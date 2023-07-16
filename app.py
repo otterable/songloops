137a97230db31fb154a5303734460fcefa507386
otterable <110373485+otterable@users.noreply.github.com>
2023-07-16 10:37:34 +0200

    works^

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import os
 import subprocess
 import shutil
 import time
+import zipfile
 from flask import Flask, request, redirect, url_for, send_from_directory
+from pydub import AudioSegment
 
 app = Flask(__name__)
 
@@ -12,7 +14,7 @@ def index():
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Ermine song loops</title>
+    <title>Ermine Song Loops</title>
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -26,7 +28,7 @@ def index():
         .container {
             max-width: 800px;
             margin: 0 auto;
-            padding: 60px;
+            padding: 20px 30px;
             background-color: #111;
             border-radius: 5px;
             box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
@@ -34,35 +36,37 @@ def index():
 
         .logo {
             text-align: center;
-            margin-bottom: 40px;
+            margin-bottom: 10px;
+            margin-top: 0;
         }
 
         .logo img {
-            width: 250px;
+            width: 200px;
         }
 
         h1 {
             text-align: center;
-            font-size: 48px;
-            margin-bottom: 40px;
+            font-size: 36px;
+            margin-bottom: 20px;
+            margin-top: 10px;
         }
 
         .form-group {
-            margin-bottom: 40px;
+            margin-bottom: 20px;
             text-align: center;
         }
 
         .form-group label {
             display: block;
-            margin-bottom: 20px;
-            font-size: 32px;
+            margin-bottom: 10px;
+            font-size: 24px;
         }
 
-        .form-group input[type="file"], .form-group input[type="text"] {
+        .form-group input[type="file"], .form-group input[type="text"], .form-group input[type="checkbox"] {
             display: block;
             width: 100%;
-            padding: 20px;
-            font-size: 32px;
+            padding: 10px;
+            font-size: 20px;
             background-color: #222;
             color: #fff;
             border: none;
@@ -72,10 +76,10 @@ def index():
         .form-group input[type="submit"] {
             background-color: #ff0;
             color: #000;
-            padding: 20px 40px;
+            padding: 10px 20px;
             border: none;
             border-radius: 5px;
-            font-size: 32px;
+            font-size: 24px;
             cursor: pointer;
             margin: 0 auto;
         }
@@ -85,10 +89,19 @@ def index():
         }
 
         .about-section {
-            margin-top: 60px;
+            margin-top: 10px;
+            margin-bottom: 10px;
             text-align: center;
-            font-size: 24px;
+            font-size: 18px;
             color: #ccc;
+        }
+
+        .usage-title {
+            font-weight: bold;
+        }
+
+        .usage-text {
+            text-align: center;
         }
     </style>
 </head>
@@ -104,31 +117,38 @@ def index():
                 <input type="file" name="music" id="music" accept=".mp3, .wav" multiple>
             </div>
             <div class="form-group">
+                <label for="triplicate">Loop 3 times</label>
+                <input type="checkbox" name="triplicate" id="triplicate" style="transform: scale(1.5);">
+            </div>
+            <div class="form-group">
                 <input type="submit" value="Upload">
             </div>
         </form>
         <form action="/process" method="post">
             <div class="form-group">
-                <label for="url">Enter YouTube URL:</label>
+                <label for="url">Or enter a YouTube URL:</label>
                 <input type="text" name="url" id="url">
+            </div>
+            <div class="form-group">
+                <label for="triplicate">Loop 3 times</label>
+                <input type="checkbox" name="triplicate" id="triplicate" style="transform: scale(1.5);">
             </div>
             <div class="form-group">
                 <input type="submit" value="Process">
             </div>
         </form>
         <div class="about-section">
-            A simple site for generating perfect song loops. Supports batch file upload. Max. filesize is 100 MB. An Ermine project.
+            A simple site for generating perfect song loops. Max. size: 100 MB. An Ermine project.
+            <div class="usage-text">Creates prefectly loopable song cuts for easy repetition.</div>
         </div>
     </div>
 </body>
 </html>
 '''
 
-# Serve the favicon file
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -137,6 +157,7 @@ def upload():
         return redirect(url_for('error'))
 
     files = request.files.getlist('music')
+    triplicate = 'triplicate' in request.form
 
     looped_files = []
     output_folder = os.path.join(app.root_path, 'temp', 'LooperOutput')
@@ -149,25 +170,9 @@ def upload():
         file_path = os.path.join(app.root_path, 'temp', filename)
         file.save(file_path)
 
-        # Run the music looping script using subprocess
-        script_path = 'pymusiclooper'  # Assuming the script is in the current directory or on the PATH
-        output_filename = f'{os.path.splitext(filename)[0]}-loop{os.path.splitext(filename)[1]}'
-        output_filepath = os.path.join(output_folder, output_filename)
-
-        command = [script_path, 'split-audio', '--path', file_path]
-
-        subprocess.run(command, cwd=app.root_path)
-
-        # Wait for the output file to be generated
-        time.sleep(2)
-
-        # Move the generated file to the 'LooperOutput' folder
-        generated_filename = filename + '-loop.wav'
-        generated_filepath = os.path.join(output_folder, generated_filename)
-
-        if os.path.exists(generated_filepath):
-            shutil.move(generated_filepath, output_filepath)
-            looped_files.append(output_filename)
+        looped_filepath = process_file(file_path, output_folder, triplicate)
+        if looped_filepath:
+            looped_files.append(looped_filepath)
 
     # Provide the download links to the user
     if len(looped_files) == 1:
@@ -220,186 +225,197 @@ def process():
             </html>
             '''
 
-        # Run the music looping script using subprocess with the URL
-        script_path = 'pymusiclooper'  # Assuming the script is in the current directory or on the PATH
-        output_folder = os.path.join(app.root_path, 'LooperOutput')
+        triplicate = 'triplicate' in request.form
+        output_folder = os.path.join(app.root_path, 'temp', 'LooperOutput')
 
-        command = [script_path, 'split-audio', '--url', url]
+        os.makedirs(output_folder, exist_ok=True)  # Create the 'LooperOutput' directory if it doesn't exist
 
-        try:
-            subprocess.run(command, cwd=app.root_path, check=True)
-
-        except subprocess.CalledProcessError as e:
-            # An error occurred during the execution of the music looping script
-            return '''
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <title>Error Processing URL</title>
-                <style>
-                    ...
-                </style>
-            </head>
-            <body>
-                <div class="message">
-                    <h1>Error Processing URL</h1>
-                    <p>An error occurred while processing the URL.</p>
-                    <p>Error Output:</p>
-                    <pre>{}</pre>
-                    <p>Error: {}</p>
-                </div>
-            </body>
-            </html>
-            '''.format(e.output, e)
-
-        except Exception as e:
-            # An exception occurred while running the subprocess command
-            return '''
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <title>Error Processing URL</title>
-                <style>
-                    body {
-                        font-family: Arial, sans-serif;
-                        background-color: #000;
-                        color: #fff;
-                        font-size: 28px;
-                        line-height: 1.5;
-                        font-weight: bold;
-                        text-align: center;
-                    }
-                    .message {
-                        margin-top: 100px;
-                    }
-                </style>
-            </head>
-            <body>
-                <div class="message">
-                    <h1>Error Processing URL</h1>
-                    <p>An error occurred while processing the URL.</p>
-                    <p>Error: {}</p>
-                </div>
-            </body>
-            </html>
-            '''.format(str(e))
-
-        # Wait for the output files to be generated
-        time.sleep(2)
-
-        # Check if any files were generated in the output folder
-        files_in_output_folder = os.listdir(output_folder)
-        if not files_in_output_folder:
-            return '''
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <title>No Files Generated</title>
-                <style>
-                    body {
-                        font-family: Arial, sans-serif;
-                        background-color: #000;
-                        color: #fff;
-                        font-size: 28px;
-                        line-height: 1.5;
-                        font-weight: bold;
-                        text-align: center;
-                    }
-                    .message {
-                        margin-top: 100px;
-                    }
-                </style>
-            </head>
-            <body>
-                <div class="message">
-                    <h1>No Files Generated</h1>
-                    <p>No looped files were generated for the given URL.</p>
-                </div>
-            </body>
-            </html>
-            '''
-
-        # Find the looped file
-        loop_filename = None
-        for filename in files_in_output_folder:
-            if filename.endswith('.opus-loop.wav'):
-                loop_filename = filename
-                break
-
-        if loop_filename is None:
-            return '''
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <title>No Loop File Found</title>
-                <style>
-                    body {
-                        font-family: Arial, sans-serif;
-                        background-color: #000;
-                        color: #fff;
-                        font-size: 28px;
-                        line-height: 1.5;
-                        font-weight: bold;
-                        text-align: center;
-                    }
-                    .message {
-                        margin-top: 100px;
-                    }
-                </style>
-            </head>
-            <body>
-                <div class="message">
-                    <h1>No Loop File Found</h1>
-                    <p>No looped file was generated for the given URL.</p>
-                </div>
-            </body>
-            </html>
-            '''
-
-        # Move the generated loop file to the output folder
-        looped_filepath = os.path.join(output_folder, loop_filename)
-        loop_filepath = os.path.join(app.root_path, 'temp', 'LooperOutput', loop_filename)
-
-        try:
-            os.makedirs(os.path.dirname(loop_filepath), exist_ok=True)
-            shutil.move(looped_filepath, loop_filepath)
-        except Exception as e:
-            return '''
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <title>Error Moving File</title>
-                <style>
-                    body {
-                        font-family: Arial, sans-serif;
-                        background-color: #000;
-                        color: #fff;
-                        font-size: 28px;
-                        line-height: 1.5;
-                        font-weight: bold;
-                        text-align: center;
-                    }
-                    .message {
-                        margin-top: 100px;
-                    }
-                </style>
-            </head>
-            <body>
-                <div class="message">
-                    <h1>Error Moving File</h1>
-                    <p>An error occurred while moving the generated file. Error: {error}</p>
-                </div>
-            </body>
-            </html>
-            '''.format(error=str(e))
-
-        # Provide the download link to the user for the looped file
-        return send_from_directory(os.path.join(app.root_path, 'temp', 'LooperOutput'), loop_filename, as_attachment=True)
+        looped_filename = process_url(url, output_folder, triplicate)
+        if looped_filename:
+            return send_from_directory(output_folder, looped_filename, as_attachment=True)
 
     return redirect(url_for('error'))
 
+def process_file(file_path, output_folder, triplicate):
+    # Run the music looping script using subprocess
+    script_path = 'pymusiclooper'  # Assuming the script is in the current directory or on the PATH
+    output_filename = f'{os.path.splitext(os.path.basename(file_path))[0]}-loop{os.path.splitext(file_path)[1]}'
+    output_filepath = os.path.join(output_folder, output_filename)
 
-import zipfile
+    command = [script_path, 'split-audio', '--path', file_path]
+
+    subprocess.run(command, cwd=app.root_path)
+
+    # Wait for the output file to be generated
+    time.sleep(2)
+
+    # Move the generated file to the 'LooperOutput' folder
+    generated_filename = os.path.basename(file_path) + '-loop.wav'
+    generated_filepath = os.path.join(output_folder, generated_filename)
+
+    if os.path.exists(generated_filepath):
+        shutil.move(generated_filepath, output_filepath)
+        
+        if triplicate:
+            triplicated_filename = triplicate_audio(output_filepath)
+            if triplicated_filename:
+                return triplicated_filename
+        
+        return output_filename
+
+    return None
+
+def process_url(url, output_folder, triplicate):
+    # Run the music looping script using subprocess with the URL
+    script_path = 'pymusiclooper'  # Assuming the script is in the current directory or on the PATH
+
+    command = [script_path, 'split-audio', '--url', url]
+
+    try:
+        subprocess.run(command, cwd=app.root_path, check=True)
+
+    except subprocess.CalledProcessError as e:
+        # An error occurred during the execution of the music looping script
+        return '''
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Error Processing URL</title>
+            <style>
+                ...
+            </style>
+        </head>
+        <body>
+            <div class="message">
+                <h1>Error Processing URL</h1>
+                <p>An error occurred while processing the URL.</p>
+                <p>Error Output:</p>
+                <pre>{}</pre>
+                <p>Error: {}</p>
+            </div>
+        </body>
+        </html>
+        '''.format(e.output, e)
+
+    except Exception as e:
+        # An exception occurred while running the subprocess command
+        return '''
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Error Processing URL</title>
+            <style>
+                ...
+            </style>
+        </head>
+        <body>
+            <div class="message">
+                <h1>Error Processing URL</h1>
+                <p>An error occurred while processing the URL.</p>
+                <p>Error: {}</p>
+            </div>
+        </body>
+        </html>
+        '''.format(str(e))
+
+    # Wait for the output files to be generated
+    time.sleep(2)
+
+    # Check if any files were generated in the output folder
+    files_in_output_folder = os.listdir(output_folder)
+    if not files_in_output_folder:
+        return '''
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>No Files Generated</title>
+            <style>
+                ...
+            </style>
+        </head>
+        <body>
+            <div class="message">
+                <h1>No Files Generated</h1>
+                <p>No looped files were generated for the given URL.</p>
+            </div>
+        </body>
+        </html>
+        '''
+
+    # Find the looped file
+    loop_filename = None
+    for filename in files_in_output_folder:
+        if filename.endswith('.opus-loop.wav'):
+            loop_filename = filename
+            break
+
+    if loop_filename is None:
+        return '''
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>No Loop File Found</title>
+            <style>
+                ...
+            </style>
+        </head>
+        <body>
+            <div class="message">
+                <h1>No Loop File Found</h1>
+                <p>No looped file was generated for the given URL.</p>
+            </div>
+        </body>
+        </html>
+        '''
+
+    # Move the generated loop file to the output folder
+    looped_filepath = os.path.join(output_folder, loop_filename)
+    loop_filepath = os.path.join(app.root_path, 'temp', 'LooperOutput', loop_filename)
+
+    try:
+        os.makedirs(os.path.dirname(loop_filepath), exist_ok=True)
+        shutil.move(looped_filepath, loop_filepath)
+    except Exception as e:
+        return '''
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Error Moving File</title>
+            <style>
+                ...
+            </style>
+        </head>
+        <body>
+            <div class="message">
+                <h1>Error Moving File</h1>
+                <p>An error occurred while moving the generated file. Error: {error}</p>
+            </div>
+        </body>
+        </html>
+        '''.format(error=str(e))
+
+    if triplicate:
+        triplicated_filename = triplicate_audio(loop_filepath)
+        if triplicated_filename:
+            return triplicated_filename
+
+    # Provide the download link to the user for the looped file
+    return loop_filename
+
+def triplicate_audio(audio_path):
+    # Load the audio file
+    audio = AudioSegment.from_file(audio_path, format='wav')
+
+    # Triplicate the audio by appending copies
+    triplicated_audio = audio + audio + audio
+
+    # Export the triplicated audio to a new file
+    triplicated_filename = os.path.splitext(os.path.basename(audio_path))[0] + '-triplicate.wav'
+    triplicated_filepath = os.path.join(app.root_path, 'temp', 'LooperOutput', triplicated_filename)
+
+    triplicated_audio.export(triplicated_filepath, format='wav')
+
+    return triplicated_filename
 
 @app.route('/download')
 def download():
@@ -410,16 +426,17 @@ def download():
 
         # Create a zip file containing the looped files
         zip_filename = 'looped_files.zip'
-        zip_filepath = os.path.join('temp', zip_filename)
+        zip_filepath = os.path.join(app.root_path, 'temp', zip_filename)
 
         with zipfile.ZipFile(zip_filepath, 'w') as zip_file:
             for filename in filenames:
-                looped_filepath = os.path.join('temp', 'LooperOutput', filename)
+                looped_filepath = os.path.join(app.root_path, 'temp', 'LooperOutput', filename)
                 zip_file.write(looped_filepath, os.path.basename(looped_filepath))
 
-        return send_from_directory('temp', zip_filename, as_attachment=True)
+        return send_from_directory(os.path.join(app.root_path, 'temp'), zip_filename, as_attachment=True)
 
     return redirect(url_for('error'))
+
 
 @app.route('/error')
 def error():
